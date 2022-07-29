6ee3a9deff5f6849b5df836901fdd9e4481298d4
hjkim20 <hjkim20@eduwill.net>
2022-07-29 10:32:47 +0900

    Initial Commit

---
+++ HozScrollTest2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HozScrollTest2
+//
+//  Created by Hyeon Jong Kim on 2022/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
